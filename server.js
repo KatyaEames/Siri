fc08860936aaf3ae2b1b124d2ded5ed829a9da92
KatyaEames <katya.eames@gmail.com>
2015-02-06 15:25:37 -0700

    server

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 var http = ('http');
 var port = 887;
 
-http.createServer(function(req, res) {
+var messages = ["Hello there.", "I'm sorry, I cannot take any requests at this time.", "I can tell you how to do that."];
 
-}).listen(port);
+var onRequest = function(req, res) {
+	if(req.method == 'GET') {
+		res.writeHead(200, {
+			'Connection': 'close',
+			'Content-Type': 'application/json',
+			'Access-Control-Allow-Origin': '*',
+
+		})
+		res.end();
+	}
+}
+
+var randomMessage = function(array) {
+	var num = Math.floor(Math.random()*(array.length));
+	return array[num]
+}
+
+http.createServer(onRequest).listen(port);
+
 console.log("listening on port" + port)
